15a8f5ecb63b88f22c72c578833d25470962bba6
Jose <jose.galea@educa.madrid.org>
2022-01-17 19:59:29 +0100

    Base de Datos modificada para el tema de las TARJETAS DE LOS J...S

--- Proyecto/__Documentacion/BBDD_TFG.sql
+++ Proyecto/__Documentacion/BBDD_TFG.sql
@@ -25,8 +25,52 @@ INSERT INTO usuario
 (id, identificador, email, contrasenna, codigoCookie, caducidadCodigoCookie, nombre, apellidos)
 VALUES
     (1, 'pLotudo', 'pepelot@gmail.com', 'pp', NULL, NULL, 'Pepe',   'Lotudo'),
-    (2, 'Josian', 'jositron3@gmail.com', 'josian', NULL, NULL, 'Josian',   'Galtor'),
+    (2, 'Josian', 'jositron2@gmail.com', 'josian', NULL, NULL, 'Josian',   'Galtor'),
     (3, 'ManCa', 'carrasMan@gmail.com', 'mc', NULL, NULL, 'Manuela',   'Carrasco'),
     (4, 'Iverma', 'dbdtodoeldia@gmail.com', 'iv', NULL, NULL, 'Ivan',   'Delas');
 
 
+
+CREATE TABLE IF NOT EXISTS videojuego (
+    id int(11) NOT NULL AUTO_INCREMENT,
+    nombre varchar(90) COLLATE utf8_spanish_ci NOT NULL,
+    identificadorFoto varchar(90) COLLATE utf8_spanish_ci NOT NULL,
+    categoriaId int(4) COLLATE utf8_spanish_ci NOT NULL,
+    KEY fk_categoriaIdIdx (categoriaId),
+    PRIMARY KEY (id)
+    )
+    ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+TRUNCATE TABLE videojuego;
+
+/*LA IDEA ES QUE el identificador de la foto sea el nombre de la foto (ej: la foto se llama AC1.jpg; pues su identificador de foto es AC1)*/
+INSERT INTO videojuego
+(id, nombre, identificadorFoto, categoriaId)
+VALUES
+    (1, '2048', '2048', 5),
+    (2, 'RUN&JUMP', 'R&J', 1),
+    (3, 'The Dungeon Game', 'dungeon', 1),
+    (4, 'Platforms World', 'platformWorld', 1),
+    (5, 'Assassin´s Creed', 'AC1', 2);
+
+
+
+CREATE TABLE IF NOT EXISTS categoriaVideojuego (
+    id int(11) NOT NULL AUTO_INCREMENT,
+    categoria varchar(90) COLLATE utf8_spanish_ci NOT NULL,
+    PRIMARY KEY (id)
+    )
+    ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+TRUNCATE TABLE categoriaVideojuego;
+
+
+INSERT INTO categoriaVideojuego
+(id, categoria)
+VALUES
+    (1, 'plataformas-2D'),
+    (2, 'Accion-Aventuras'),
+    (3, 'Misterio'),
+    (4, 'Survival-Horror'),
+    (5, 'puzzle'),
+    (6, 'RPG'),
+    (7, 'Arcade'),
+    (8, 'Sandbox');
